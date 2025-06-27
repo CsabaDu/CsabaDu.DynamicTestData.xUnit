8cb0a2f38c66b207511dc676d2277f1ba9aad67d
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-06-27 10:42:11 +0200

    dependency update

--- Attributes/MemberTestDataAttribute.cs
+++ Attributes/MemberTestDataAttribute.cs
@@ -1,6 +1,7 @@
 ﻿// SPDX-License-Identifier: MIT
 // Copyright (c) 2025. Csaba Dudas (CsabaDu)
 
+
 namespace CsabaDu.DynamicTestData.xUnit.Attributes;
 
 public sealed class MemberTestDataAttribute(
--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,7 +1,7 @@
 ﻿// SPDX-License-Identifier: MIT
 // Copyright (c) 2025. Csaba Dudas (CsabaDu)
 
+global using CsabaDu.DynamicTestData.DataStrategyTypes.Interfaces;
 global using CsabaDu.DynamicTestData.TestDataRows.Interfaces;
 global using System.Reflection;
 global using Xunit;
-
