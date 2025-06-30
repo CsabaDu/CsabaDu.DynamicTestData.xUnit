1b591bb6e6e8f501d84bc1218315640fc181a450
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-06-30 15:20:04 +0200

    Attribute extension

--- Attributes/MemberTestDataAttributeBase.cs
+++ Attributes/MemberTestDataAttributeBase.cs
@@ -34,7 +34,77 @@ public abstract class MemberTestDataAttributeBase(
         }
 
         return testData.ToParams(
-            ArgsCode,
+            getArgsCode(),
             testData is IExpected)!;
+
+        #region Local methods
+        ArgsCode getArgsCode()
+        {
+            try
+            {
+                var testMethodType = (MemberType?.DeclaringType)
+                    ?? throw new InvalidOperationException(
+                        "Test method type is null");
+
+                object memberValue = getMemberValue(
+                    testMethodType,
+                    BindingFlags.Static |
+                    BindingFlags.Public |
+                    BindingFlags.NonPublic)
+                    ?? throw new InvalidOperationException(
+                        "static data source member is not found " +
+                        $"in the test method {testMethodType.Name}");
+
+                var argCodeProperty = MemberType?.GetProperty("ArgsCode");
+
+                if (argCodeProperty?.GetValue(memberValue) is ArgsCode argsCode)
+                {
+                    return argsCode;
+                }
+
+                throw new InvalidOperationException(
+                    "'ArgsCode' property is not found in the member " +
+                    $"{MemberType?.Name}.{MemberName}");
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException(
+                    "Failed to retrieve ArgsCode from " +
+                    $"{MemberType?.Name}.{MemberName}",
+                    ex is TargetInvocationException tiex ?
+                        tiex.InnerException
+                        : ex);
+            }
+        }
+
+        object? getMemberValue(Type testMethodType, BindingFlags flags)
+        {
+            if (testMethodType.GetProperty(MemberName, flags)
+                is { PropertyType: var propertyType } propertyInfo
+                && propertyType == MemberType)
+            {
+                return propertyInfo.GetValue(null);
+            }
+
+            if (testMethodType.GetMethod(MemberName, flags,
+                null,
+                Type.EmptyTypes,
+                null)
+                is { ReturnType: var returnType } methodInfo
+                && returnType == MemberType)
+            {
+                return methodInfo.Invoke(null, null);
+            }
+
+            if (testMethodType.GetField(MemberName, flags)
+                is { FieldType: var fieldType } fieldInfo
+                && fieldType == MemberType)
+            {
+                return fieldInfo.GetValue(null);
+            }
+
+            return null;
+        }
+        #endregion
     }
 }
