65752c371089f0193566e3f1462ee8c0477a2b10
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-12-06 08:55:11 +0100

    cleanup

--- Attributes/MemberTestDataAttribute.cs
+++ Attributes/MemberTestDataAttribute.cs
@@ -1,8 +1,40 @@
 ﻿// SPDX-License-Identifier: MIT
 // Copyright (c) 2025. Csaba Dudas (CsabaDu)
 
-
 namespace CsabaDu.DynamicTestData.xUnit.Attributes;
+
+public abstract class MemberTestDataAttributeBase(
+    string memberName, object[]? args)
+: MemberDataAttributeBase(
+    memberName, args)
+{
+    protected override object[] ConvertDataItem(
+        MethodInfo testMethod,
+        object item)
+    {
+        if (item is not ITestData testData)
+        {
+            if (item is not ITestDataRow testDataRow)
+            {
+                return item switch
+                {
+                    null => null!,
+                    object?[] args => args as object[],
+                    _ => throw new ArgumentException(
+                        $"'{MemberName}' member of '{MemberType.Name}' " +
+                        "yielded an item that is not an 'object[]' " +
+                        "or an 'ITestData' or 'ITestDataRow' instance"),
+                };
+            }
+
+            testData = testDataRow.GetTestData();
+        }
+
+        return testData.ToParams(
+            ArgsCode.Instance,
+            PropsCode.Expected)!;
+    }
+}
 
 public sealed class MemberTestDataAttribute(
     string memberName,
